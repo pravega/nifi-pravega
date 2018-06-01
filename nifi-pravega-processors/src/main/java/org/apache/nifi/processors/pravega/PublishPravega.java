20729793a5d22ada5ce5d5a91e4e7c4471087226
Claudio Fahey <claudio.fahey@emc.com>
2018-06-01 23:04:13 +0000

    Simplest publish to Pravega working

--- nifi-pravega-processors/src/main/java/org/apache/nifi/processors/pravega/PublishPravega.java
+++ nifi-pravega-processors/src/main/java/org/apache/nifi/processors/pravega/PublishPravega.java
@@ -24,12 +24,14 @@ import io.pravega.client.stream.ScalingPolicy;
 import io.pravega.client.stream.StreamConfiguration;
 import io.pravega.client.stream.impl.ByteArraySerializer;
 import org.apache.nifi.annotation.behavior.*;
+import org.apache.nifi.annotation.lifecycle.OnStopped;
 import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.annotation.lifecycle.OnScheduled;
 import org.apache.nifi.annotation.documentation.CapabilityDescription;
 import org.apache.nifi.annotation.documentation.SeeAlso;
 import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.logging.ComponentLog;
 import org.apache.nifi.processor.*;
 import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.processor.io.InputStreamCallback;
@@ -40,14 +42,12 @@ import org.apache.nifi.stream.io.StreamUtils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
 
 @Tags({"Pravega", "Put", "Send", "Message"})
 @CapabilityDescription("Sends the contents of a FlowFile as a message to Pravega.")
@@ -56,14 +56,32 @@ import java.util.concurrent.TimeUnit;
 @ReadsAttributes({@ReadsAttribute(attribute="", description="")})
 @WritesAttributes({@WritesAttribute(attribute="", description="")})
 public class PublishPravega extends AbstractProcessor {
+    protected ComponentLog logger;
+    protected EventStreamWriter<byte[]> cachedWriter;
 
-    public static final PropertyDescriptor MY_PROPERTY = new PropertyDescriptor
-        .Builder().name("MY_PROPERTY")
-        .displayName("My property")
-        .description("Example Property")
-        .required(true)
-        .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
-        .build();
+    static final PropertyDescriptor PROP_CONTROLLER = new PropertyDescriptor.Builder()
+            .name("controller")
+            .displayName("Pravega Controller URI")
+            .description("The URI of the Pravega controller.")
+            .required(true)
+            .addValidator(StandardValidators.NON_BLANK_VALIDATOR)
+            .build();
+
+    static final PropertyDescriptor PROP_SCOPE = new PropertyDescriptor.Builder()
+            .name("scope")
+            .displayName("Pravega Scope")
+            .description("The name of the Pravega scope.")
+            .required(true)
+            .addValidator(StandardValidators.NON_BLANK_VALIDATOR)
+            .build();
+
+    static final PropertyDescriptor PROP_STREAM = new PropertyDescriptor.Builder()
+            .name("stream")
+            .displayName("Pravega Stream")
+            .description("The name of the Pravega stream.")
+            .required(true)
+            .addValidator(StandardValidators.NON_BLANK_VALIDATOR)
+            .build();
 
     static final Relationship REL_SUCCESS = new Relationship.Builder()
         .name("success")
@@ -81,8 +99,13 @@ public class PublishPravega extends AbstractProcessor {
 
     @Override
     protected void init(final ProcessorInitializationContext context) {
+        logger = getLogger();
+        logger.info("PublishPravega.init");
+
         final List<PropertyDescriptor> descriptors = new ArrayList<PropertyDescriptor>();
-        descriptors.add(MY_PROPERTY);
+        descriptors.add(PROP_CONTROLLER);
+        descriptors.add(PROP_SCOPE);
+        descriptors.add(PROP_STREAM);
         this.descriptors = Collections.unmodifiableList(descriptors);
 
         final Set<Relationship> relationships = new HashSet<Relationship>();
@@ -103,12 +126,24 @@ public class PublishPravega extends AbstractProcessor {
 
     @OnScheduled
     public void onScheduled(final ProcessContext context) {
+        logger.info("PublishPravega.onScheduled");
+    }
 
+    @OnStopped
+    public void onStop(final ProcessContext context) {
+        synchronized (this) {
+            logger.info("PublishPravega.onStop");
+            if (cachedWriter != null) {
+                cachedWriter.close();
+                cachedWriter = null;
+            }
+        }
     }
 
     @Override
     public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException {
-        System.out.println("PublishPravega.onTrigger: BEGIN");
+//        System.out.println("PublishPravega.onTrigger: BEGIN");
+        logger.info("PublishPravega.onTrigger: BEGIN");
 
         final List<FlowFile> flowFiles = session.get(FlowFileFilters.newSizeBasedFilter(250, DataUnit.KB, 500));
         if (flowFiles.isEmpty()) {
@@ -116,29 +151,18 @@ public class PublishPravega extends AbstractProcessor {
         }
 
         try {
-            URI controllerURI = new URI("tcp://10.246.21.230:9090");
-            String scope = "nifitest1";
-            String streamName = "fromnifi";
-            if (false) {
-                StreamManager streamManager = StreamManager.create(controllerURI);
-                streamManager.createScope(scope);
-                StreamConfiguration streamConfig = StreamConfiguration.builder()
-                        .scalingPolicy(ScalingPolicy.fixed(2))
-                        .build();
-                streamManager.createStream(scope, streamName, streamConfig);
-            }
-
-            ClientFactory clientFactory = ClientFactory.withScope(scope, controllerURI);
-            EventStreamWriter<byte[]> writer = clientFactory.createEventWriter(
-                streamName,
-                new ByteArraySerializer(),
-                EventWriterConfig.builder().build());
+            EventStreamWriter<byte[]> writer = getWriter(context);
 
             for (final FlowFile flowFile : flowFiles) {
                 if (!isScheduled()) {
                     // If stopped, re-queue FlowFile instead of sending it
                     session.transfer(flowFile);
                     continue;
+                }
+
+                String routingKey = flowFile.getAttribute("pravega.routing.key");
+                if (routingKey == null) {
+                    routingKey = "";
                 }
 
                 // do the read
@@ -149,22 +173,60 @@ public class PublishPravega extends AbstractProcessor {
                         StreamUtils.fillBuffer(in, messageContent, true);
                     }
                 });
+                logger.info("routingKey={}, size={}, messageContent={}",
+                        new Object[]{routingKey, flowFile.getSize(), new String(messageContent)});
 
-                String routingKey = "";
+                // TODO: get routing key from attribute
                 final CompletableFuture writeFuture = writer.writeEvent(routingKey, messageContent);
+                // TODO: only do wait after all messages have been sent.
                 writeFuture.get();
             }
-            writer.close();
-            clientFactory.close();
         }
         catch (Exception e) {
+            logger.error(e.getMessage());
             throw new ProcessException(e);
         }
 
         // Transfer any successful FlowFiles.
+        // TODO: do this when future completes?
         for (FlowFile success : flowFiles) {
             session.transfer(success, REL_SUCCESS);
         }
-        System.out.println("PublishPravega.onTrigger: END");
+//        System.out.println("PublishPravega.onTrigger: END");
+        logger.info("PublishPravega.onTrigger: END");
+    }
+
+    protected EventStreamWriter<byte[]> getWriter(ProcessContext context) {
+        synchronized (this) {
+            if (cachedWriter == null) {
+                try {
+                    URI controllerURI = new URI(context.getProperty(PROP_CONTROLLER).getValue());
+                    String scope = context.getProperty(PROP_SCOPE).getValue();
+                    String streamName = context.getProperty(PROP_STREAM).getValue();
+
+                    StreamManager streamManager = StreamManager.create(controllerURI);
+                    streamManager.createScope(scope);
+                    StreamConfiguration streamConfig = StreamConfiguration.builder()
+                            .scalingPolicy(ScalingPolicy.fixed(2))
+                            .build();
+                    streamManager.createStream(scope, streamName, streamConfig);
+                    streamManager.close();
+
+                    // TODO: do I need to close clientFactory?
+                    ClientFactory clientFactory = ClientFactory.withScope(scope, controllerURI);
+                    EventStreamWriter<byte[]> writer = clientFactory.createEventWriter(
+                            streamName,
+                            new ByteArraySerializer(),
+                            EventWriterConfig.builder().build());
+                    cachedWriter = writer;
+                    return writer;
+                } catch (Exception e) {
+                    logger.error(e.getMessage());
+                    throw new RuntimeException(e);
+                }
+            } else {
+                return cachedWriter;
+            }
+        }
     }
 }
